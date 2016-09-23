79933cc67c2f7321c203826fe7bd99721271da3a
Sthefany <Sthefany>
2016-09-23 09:31:19 -0500

    Subiendo version 2

--- js/app.js
+++ js/app.js
@@ -18,9 +18,34 @@ window.onload = function(){
         
     }
     
+     function nuevaTarjeta(){
+    nuevaTarea.style.display = "none";  
+	var contenedorTarjeta = document.createElement("div");
+	var newTarjeta = contenedor.appendChild(contenedorTarjeta);
+    var titulo = document.getElementById("nuevoInput").value;        
+    var contenedorTitulo = document.createElement("div");   
+    contenedorTitulo.classList.add("contenedorTitulo");  
+    contenedorTitulo.textContent = titulo;    
+	var enlace = document.createElement("a"); 
+    contenedorTitulo.appendChild(enlace);
+    enlace.innerHTML = "Agregar una nueva Tarea...";   
+    contenedorTarjeta.appendChild(contenedorTitulo); 
+	contenedorTarjeta.appendChild(enlace);    
+}
+    
+   
+    /* Dar Enter y crear nueva Tarea 
+    var darEnter = function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        alert("Acabas de presionar Enter");  
+    }
+        }
+    */ 
+    
     //Eventos
     //tareaInput.addEventListener("keydown",darEnter);
     contentAdd.addEventListener("click",crearLista);
-    //boton.addEventListener("click",nuevaTarjeta);
+    boton.addEventListener("click",nuevaTarjeta);
         
 };
