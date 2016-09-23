6e65b4ed6d28d28b89e14698435e7459e3624b43
Sthefany <Sthefany>
2016-09-23 09:27:59 -0500

    Subiendo primera version

---
+++ js/app.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+    /* Variables */
+    var contenedor = document.getElementById("contenedor"),
+        contentAdd = document.getElementById("contentAdd"),
+        nuevoInput = document.createElement("input"),
+        boton = document.getElementById("btnSave");
+        nuevoInput.setAttribute("id" , "nuevoInput");
+        var nuevaTarea = document.createElement("form");
+    
+    /* Funciones */
+    function crearLista() {
+        contentAdd.style.display = "none";
+        contenedor.insertBefore(nuevaTarea, contenedor.childNodes[0]);
+        nuevaTarea.insertBefore(nuevoInput, nuevaTarea.childNodes[0]).classList.add("styleinput");
+        boton.style.display = "block";
+        nuevaTarea.appendChild(boton);
+        contenedor.classList.add("tarjeta");
+        
+    }
+    
+   
+    /* Dar Enter y crear nueva Tarea 
+    var darEnter = function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        alert("Acabas de presionar Enter");  
+    }
+        }
+    */ 
+    
+    //Eventos
+    //tareaInput.addEventListener("keydown",darEnter);
+    contentAdd.addEventListener("click",crearLista);
+    //boton.addEventListener("click",nuevaTarjeta);
+        
+};
